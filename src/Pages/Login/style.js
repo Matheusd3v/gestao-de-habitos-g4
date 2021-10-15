70be055f6a7438adfb513d042998933ed49f23be
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-10-15 10:50:44 -0300

    feat: Adding the material ui to the form

--- src/Pages/Login/style.js
+++ src/Pages/Login/style.js
@@ -4,4 +4,28 @@ export const Container = styled.main`
   margin: 0;
   min-height: 100vh;
   background-color: #293241;
+  display: flex;
+  align-items: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    height: 200px;
+    border-radius: 10px;
+    align-items: center;
+    justify-content: space-around;
+    background: #98c1d9;
+    width: fit-content;
+    padding: 15px;
+    /* fieldset {
+      border-color: #fff;
+    }
+    input:hover {
+      border-color: red;
+    }
+    label {
+      color: #fff;
+    } */
+  }
 `;
