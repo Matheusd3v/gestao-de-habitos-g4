23a37da1b1e624257927a0ee4d0fff4647844eaa
Aldenivan Jr <aldenivan1207@gmail.com>
2021-10-15 11:18:21 -0300

    feat/Creating form validations

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,15 +1,50 @@
-import { TextField } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const schema = yup.object().shape({
+    user: yup.string().required("Nome de usuário obrigatório"),
+
+    email: yup.string().email("Email Inválido").required("Email obrigátório"),
+
+    emailConfirm: yup
+      .string()
+      .oneOf([yup.ref("email")], "Emails diferentes")
+      .required("Confirmação de email obrigatória"),
+
+    password: yup.string().required("Senha Obrigatória"),
+
+    passwordConfirm: yup.string
+      .apply()
+      .oneOf([yup.ref("password")], "Senhas Diferentes")
+      .required("Confirmação de senha obrigatória"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <TextField
-          label="Name"
+          label="Username"
           variant="outlined"
           margin="normal"
           size="small"
           color="primary"
+          {...register("user")}
+          helperText={errors.user?.message}
         />
         <TextField
           label="Email"
@@ -17,6 +52,8 @@ const Register = () => {
           margin="normal"
           size="small"
           color="primary"
+          {...register("email")}
+          helperText={errors.email?.message}
         />
         <TextField
           label="Confirm email"
@@ -24,6 +61,8 @@ const Register = () => {
           margin="normal"
           size="small"
           color="primary"
+          {...register("emailConfirm")}
+          helperText={errors.emailConfirm?.message}
         />
         <TextField
           label="Password"
@@ -32,6 +71,8 @@ const Register = () => {
           size="small"
           color="primary"
           type="password"
+          {...register("password")}
+          helperText={errors.password?.message}
         />
         <TextField
           label="Confirm password"
@@ -40,6 +81,8 @@ const Register = () => {
           size="small"
           color="primary"
           type="password"
+          {...register("passworConfirm")}
+          helperText={errors.passwordConfirm?.message}
         />
         <button>Cadastrar-se</button>
       </form>
