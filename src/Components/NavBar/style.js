3ffcb0a3ad7c72bd0b4f1c61b11329594661f5f5
Matheus <matheus.g.almeida21@gmail.com>
2021-10-15 13:41:43 -0300

    feat: Added nav bar component

--- src/Components/NavBar/style.js
+++ src/Components/NavBar/style.js
@@ -1,11 +1,12 @@
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Button, MenuList, Toolbar } from "@material-ui/core";
 import styledEngine from "@material-ui/styled-engine";
 import styled from 'styled-components'
 
 
 
 export const HeaderBar = styledEngine(AppBar)({
-    background: '#3D5A80'
+    background: '#3D5A80',
+    boxShadow: '0px 10px 19px -5px rgba(0,0,0,0.78)'
 })
 
 export const HeaderToolBar = styled.div`
@@ -20,14 +21,18 @@ export const HeaderToolBar = styled.div`
         margin: 0;
     }
 
-    .notLog {
-        width: 65%;
+    .iconMenu {
+        display: none;
+    }
+
+    .navbar {
+        width: 64%;
         display: flex;
         align-items: center;
         justify-content: space-between;
         
-        .linksNotLog {
-            width: 150px;
+        .links {
+            width: 180px;
             display: flex;
             justify-content: space-between;
 
@@ -43,7 +48,87 @@ export const HeaderToolBar = styled.div`
                     transition: 0.4s;
                 }
             }
+
+            @media (max-width: 1024px) {
+                display: none;
+            }
+        }
+
+        .linksLogin {
+            width: 300px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            button {                
+                color: #FFFFFF;                
+
+                :hover {
+                    color: #FF715B;
+                    cursor: pointer;
+                    transition: 0.4s;
+                }
+            }
+
+            a {                
+                text-decoration: none;
+                color: #FFFFFF;
+                font-size: 19px;
+                font-weight: 600;
+
+                :hover {
+                    color: #FF715B;
+                    cursor: pointer;
+                    transition: 0.4s;
+                }            
+            }
+
+            @media (max-width: 1024px) {
+                display: none;
+            }
         }
     }
+
+    .isLog {
+        width: 100%;
+        padding: 0 50px;
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+
+        button {
+            color: #FFFFFF;
+            :hover {
+                color: #FF715B;
+            }
+        }
+    }
+
+    @media (max-width: 1024px){
+        justify-content: space-between;
+        padding: 0 50px;
+
+        .iconMenu {
+            display: block;
+        } 
+        
+    }
+
+    @media (max-width: 510px) {
+        height: 100px;
+    }
 `
+
+export const ButtonLog = styledEngine(Button)({
+
+})
+
+export const MenuLists = styled(MenuList)({
+    width: '100%',
+    padding: '0',
+    a : {
+        textDecoration: 'none',
+        color: '#FFFFFF',        
+    }
+})
 
--- src/GlobalStyle/index.js
+++ src/GlobalStyle/index.js
@@ -5,6 +5,38 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding:0;
     }
+
+    .MuiMenu-root {
+        margin-top: 30px;
+        display: flex;
+    }
+
+    .MuiMenu-list {
+        background: #3D5A80;
+        color: #FFFF;
+        height: 500px;
+
+        ul {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-evenly;
+            height: 100%;
+        }
+    }
+
+    .MuiMenu-paper {
+        width: 90%;
+        height: 500px;
+    }
+
+    .css-kk1bwy-MuiButtonBase-root-MuiMenuItem-root {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        font-size: 25px;
+        font-weight: bolder;
+    }
 `
 
 export default GlobalStyle;
