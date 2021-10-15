e5e94c45bcf4564d9e34a50a973e0c5943f6f89b
Aldenivan Jr <aldenivan1207@gmail.com>
2021-10-15 18:35:48 -0300

    feat/Initial structure of the groups page

--- src/Pages/Groups/index.js
+++ src/Pages/Groups/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import Button from "../../Components/ButtonDefault";
+import api from "../../Services/api";
 
 const GroupsPage = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+  const [isgroups, setIsGroups] = useState(false);
+  const [allGroups, setAllGroups] = useState([]);
 
-export default GroupsPage
+  return (
+    <>
+      <h1>Grupos</h1>
+      <Button onClick={() => setIsGroups(!isgroups)}>
+        {isgroups ? "Ver seus grupos" : "Ver todos os grupos"}
+      </Button>
+      <hr />
+      {isgroups ? <h3>Todos os grupos</h3> : <h3>Seus grupos</h3>}
+      <hr />
+      <div>
+        <div>
+          <p>+</p>
+        </div>
+        <p>Criar novo grupo</p>
+      </div>
+
+      <ul>
+        {allGroups.map((group) => (
+          <li key={group.id}>{group.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GroupsPage;
