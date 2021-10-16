6b74f3004c8f818d6219c02284f00e5ac7f8e8c1
gustavo <go8895806@gmail.com>
2021-10-16 16:28:07 -0300

    .

--- src/Pages/Groups/index.js
+++ src/Pages/Groups/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import PopPup from '../../Components/teste1'
 
 const GroupsPage = () => {
     return (
         <>
-            
+            <PopPup/>
         </>
     )
 }
