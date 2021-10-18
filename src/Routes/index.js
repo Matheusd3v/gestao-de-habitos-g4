2619cd9243fa8b25b71168dc1bec76a4dd2ea19f
Aldenivan Jr <aldenivan1207@gmail.com>
2021-10-18 11:26:05 -0300

    feat/Creating routes to groups the user belongs to

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,33 +1,36 @@
-import React from 'react'
-import { Route, Switch } from 'react-router'
-import GroupsPage from '../Pages/Groups'
-import HabitsPage from '../Pages/Habits'
-import Home from '../Pages/Home'
+import React from "react";
+import { Route, Switch } from "react-router";
+import GroupsPage from "../Pages/Groups";
+import HabitsPage from "../Pages/Habits";
+import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
 
 const Routers = () => {
-    return (
-        <>
-            <Switch>
-                <Route exact path='/'>
-                    <Home/>
-                </Route>
-                <Route path='/register'>
-                    <Register />
-                </Route>
-                <Route path='/login'>
-                  <Login />
-                </Route>
-                <Route path='/habits'>
-                    <HabitsPage/>
-                </Route>
-                <Route path='/groups'>
-                    <GroupsPage/>
-                </Route>
-            </Switch>
-        </>
-    )
-}
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/habits">
+          <HabitsPage />
+        </Route>
+        <Route path="/groups">
+          <GroupsPage />
+        </Route>
+        <Route path="/groups/subscriptions">
+          <GroupsPage />
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
 export default Routers;
