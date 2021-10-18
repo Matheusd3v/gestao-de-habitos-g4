45bef3a87b140ac1959baacb09f099fc5cef5feb
Aldenivan Jr <aldenivan1207@gmail.com>
2021-10-18 09:15:19 -0300

    feat/Searching listing of all groups

--- src/Pages/Groups/index.js
+++ src/Pages/Groups/index.js
@@ -1,26 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../Components/ButtonDefault";
 import api from "../../Services/api";
 
 const GroupsPage = () => {
-  const [isgroups, setIsGroups] = useState(false);
+  const [isGroups, setIsGroups] = useState(false);
   const [allGroups, setAllGroups] = useState([]);
+  const [nextYourGroups, setNextYourGroups] = useState([]);
+  const [nextAllGroups, setNextAllGroups] = useState(
+    "https://kenzie-habits.herokuapp.com/groups/"
+  );
 
-  const allGroupsFunction = () => {
-    api
-      .get("/groups/")
-      .then((response) => setAllGroups(response.data.results))
+  useEffect(() => {
+    fetch(nextAllGroups)
+      .then((response) => response.json())
+      .then((response) => setAllGroups([...allGroups, response.results]))
       .catch((err) => console.log(err));
-  };
+  }, [nextAllGroups]);
+
+  useEffect(() => {
+    fetch(nextAllGroups)
+      .then((response) => response.json())
+      .then((response) =>
+        response.next ? setNextAllGroups(response.next) : null
+      )
+      .catch((err) => console.log(err));
+  }, [nextAllGroups]);
 
   return (
     <>
       <h1>Grupos</h1>
-      <Button onClick={() => setIsGroups(!isgroups)}>
-        {isgroups ? "Ver seus grupos" : "Ver todos os grupos"}
+      <Button onClick={() => setIsGroups(!isGroups)}>
+        {isGroups ? "Ver seus grupos" : "Ver todos os grupos"}
       </Button>
       <hr />
-      {isgroups ? <h3>Todos os grupos</h3> : <h3>Seus grupos</h3>}
+      {isGroups ? <h3>Todos os grupos</h3> : <h3>Seus grupos</h3>}
       <hr />
       <div>
         <div>
@@ -29,15 +42,13 @@ const GroupsPage = () => {
         <p>Criar novo grupo</p>
       </div>
 
-      {/* <Button onClick={allGroupsFunction}>Teste</Button>
-
-      {console.log(allGroups)} */}
-
-      <ul>
-        {allGroups.map((group) => (
-          <li key={group.id}>{group.name}</li>
-        ))}
-      </ul>
+      {isGroups ? (
+        <ul>
+          {allGroups.map((page) =>
+            page.map((group) => <li key={group.id}>{group.name}</li>)
+          )}
+        </ul>
+      ) : null}
     </>
   );
 };
