cd277c693d1ecf7d6a725a99e68796a110728a25
Matheus <matheus.g.almeida21@gmail.com>
2021-10-18 21:39:55 -0300

    feat: Added form to edit profile

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,24 +1,83 @@
-import { AirplanemodeActive } from '@material-ui/icons'
+import { TextField } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import api from '../../Services/api'
-import { ProfileContainer } from './style'
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { FormEditProfile, ProfileContainer } from './style'
+import ButtonDefault from '../../Components/ButtonDefault';
 
 const ProfilePage = () => {
     const [name, setName] = useState('')
+    const [showOne, setShowOne] = useState(true)
 
     const { id } = useParams()
+
+    const formSchema = yup.object().shape({
+        username: yup.string(),
+        email: yup.string().email()
+    })
 
     useEffect(() => {
         api
             .get(`/users/${id}/`)
             .then((response) => setName(response.data.username))
             .catch((e) => console.log(e))
+
+        // api.post()
     }, [])
 
+    const { register, handleSubmit, formState: { errors }, } = useForm({
+        resolver: yupResolver(formSchema)
+    });
+
+    const changeBtn = () => {
+        setShowOne(!showOne)
+    }
+
+    const onSubmitFunction = (data) => {
+        console.log(data)
+        changeBtn()
+    }
+
     return (
-        <ProfileContainer>
+        <ProfileContainer
+        // initial={{opacity:0 , x:-300}}
+        // transition={{ duration: 1}}
+        // animate={{opacity:1, x: 0}} 
+        >
             <h1> Bem vindo(a), {name}!</h1>
+            <div>
+                <FormEditProfile onSubmit={handleSubmit(onSubmitFunction)}>
+                    <h3>Editar informações</h3>
+                    <TextField 
+                        label="Nome"
+                        margin="normal"
+                        variant="outlined"
+                        size="small"
+                        color="primary"
+                        {...register("username")}
+                        error={!!errors.username}
+                        helperText={errors.username?.message}
+                    />
+
+                    <TextField 
+                        label="Email"
+                        margin="normal"
+                        variant="outlined"
+                        size="small"
+                        color="primary"
+                        {...register("email")}
+                        error={!!errors.email}
+                        helperText={errors.email?.message}
+                    />
+
+                
+                    {showOne && <ButtonDefault type='submit'>Salvar</ButtonDefault>}
+                </FormEditProfile>
+                {!showOne && <ButtonDefault callback={changeBtn}>Editar</ButtonDefault>}
+            </div>
         </ProfileContainer>
     )
 }
--- src/Pages/Profile/style.js
+++ src/Pages/Profile/style.js
@@ -1,14 +1,43 @@
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
-export const ProfileContainer = styled.div`
+export const ProfileContainer = styled(motion.div)`
     min-height: calc(100vh - 70px);
     width:100%;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
 
     h1 {
-        margin: 0;
-        margin-top: 15px;
+        margin: 25px 0;
         color: #FFFFFF;
     }
+
+    div {
+        background: #FFFFFF;
+        display: flex;
+        flex-direction: column;
+    }
+
+    div button {
+        width: 130px;
+        height: 35px;
+        font-size: 20px;
+        padding: 0;        
+    }
+
+`
+
+export const FormEditProfile = styled.form`
+    background: #FFFFFF;
+    width: 250px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 0 25px;
+    padding-top: 20px;
+
 `
