fc4f965a09eaf24a3a5dd235a35250b9c9af31e9
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-10-21 11:58:35 -0300

    feat: Adding logo and editing navbar

--- src/Components/NavBar/style.js
+++ src/Components/NavBar/style.js
@@ -1,60 +1,60 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const HeaderBar =  styled.header`
-    width: 100%;
-    height: 70px;
-    background: #3D5A80;
-    display: flex;
-    flex-wrap: nowrap;
-    align-items: center;
-    justify-content: flex-end;
-    padding: 5px 40px;
-    box-sizing: border-box;
-    box-shadow: 0px 7px 21px -4px rgba(0,0,0,0.53);
+export const HeaderBar = styled.header`
+  width: 100%;
+  height: 70px;
+  background: #3d5a80;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px 40px;
+  box-sizing: border-box;
+  box-shadow: 0px 7px 21px -4px rgba(0, 0, 0, 0.53);
 
-    button {
-        display: none;
+  button {
+    display: none;
+  }
+
+  button {
+    background: transparent;
+    border: none;
+    font-size: 18px;
+    color: #ffffff;
+    cursor: pointer;
+    border: 1px solid #ffffff;
+    padding: 5px 10px;
+    border-radius: 5px;
+  }
+
+  @media screen and (max-width: 800px) {
+    .notShowMob {
+      display: none;
     }
 
     button {
-        background: transparent;
-        border: none;
-        font-size: 18px;
-        color: #FFFFFF;
-        cursor: pointer;
-        border: 1px solid #FFFFFF;
-        padding: 5px 10px;
-        border-radius: 5px;
+      display: flex;
+      align-items: center;
+      height: 60%;
     }
-
-   @media screen and (max-width: 800px) {
-       .notShowMob {
-           display: none;
-       }
-
-       button {
-            display: flex;
-            align-items: center;
-            height: 60%;
-       }
-   }
-`
+  }
+`;
 
 export const NameContainer = styled.div`
-    margin-right: 15%;
+  margin-right: 15%;
+  display: flex;
+  align-items: center;
+  margin-left: 50px;
+  img {
+    width: 75px;
+  }
 
-    h1 {
-        margin: 0;
-        color: #FFFFFF;
-    }
+  @media (max-width: 915px) {
+    width: 250px;
+    margin-right: 5%;
+  }
 
-    @media (max-width: 915px) {
-        width: 250px;
-        margin-right: 5%;
-    }
-
-    @media (max-width: 820px) {
-        margin: 0 auto;
-    }
-
-`
+  @media (max-width: 820px) {
+    margin: 0 auto;
+  }
+`;
