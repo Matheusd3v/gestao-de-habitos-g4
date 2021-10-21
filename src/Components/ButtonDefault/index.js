8a4bec8177918f69b63fb166b790acb489f0e13a
Matheusd3v <77028352+Matheusd3v@users.noreply.github.com>
2021-10-21 13:15:40 -0300

    Revert "Develop"

--- src/Components/ButtonDefault/index.js
+++
@@ -1,7 +0,0 @@
-import "./style.js";
-import { Button } from "./style.js";
-const ButtonDefault = ({ children, callback }) => {
-  return <Button onClick={callback}>{children}</Button>;
-};
-
-export default ButtonDefault;
